6dd3941ac1675d63dea33a466b146f767da475d2
Waleed Ahmed <sawasta556@gmail.com>
2024-08-31 21:24:33 +0500

    Debugging

--- VideoTube/src/controllers/user.controller.js
+++ VideoTube/src/controllers/user.controller.js
@@ -27,28 +27,42 @@ const registerUser = asyncHandler( async (req,res) => {
       throw new ApiError(400, "All fields are required")
     } 
 
-  const existedUser =   User.findOne({
+  const existedUser = await  User.findOne({
       $or: [{ username }, { email }]
     })
 
     if(existedUser){
      throw new ApiError(409, "User with email or username already exists.")
     }
-      
-    const avatarLocalPath =  req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-    if(!avatarLocalPath){
+    let coverImageLocalPath;
+       
+    const avatarLocalPath =  req.files?.avatar[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+     coverImageLocalPath = req.files.coverImage[0].path
+  }
+
+
+     console.log(avatarLocalPath);
+     console.log(coverImageLocalPath);
+     
+
+    if(!avatarLocalPath ){
       throw new ApiError(400, "Avatar File is required")
     }
+
+
 
  const avatar = await uploadOnCloudinary(avatarLocalPath)
  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
- if(avatar){
+ console.log(avatar)
+  
+ if(!avatar){
   throw new ApiError(400, "Avatar upload failed")
  }
-  
+
 
 const user = await User.create({
   fullName,
